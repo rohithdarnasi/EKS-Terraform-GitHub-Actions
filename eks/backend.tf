06aa8d4e3686e0ce6d605bcc8d2f11add1a3c052
Darnasi Rohith <73937958+rohithdarnasi@users.noreply.github.com>
2026-05-15 22:19:39 +0530

    Fix bucket declaration in backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "bucket = "rohith-terraform-state-buckett"
+    bucket         = "rohith-terraform-state-buckett"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
